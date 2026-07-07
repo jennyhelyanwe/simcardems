78b5429f92e987541eea58faecb2a8e09a88ceed
jennyhelyanwe <jenny.helyanwe@gmail.com>
2026-07-07 18:48:49 +0100

    Local Docker setup

---
+++ start_docker.sh
@@ -0,0 +1,5 @@
+docker run --rm -it \
+  -v /Users/jenny/PycharmProjects/simcardems:/repo/simcardems-dev \
+  -w /repo/simcardems-dev/simulations \
+  ghcr.io/computationalphysiology/simcardems:latest bash
+ 
