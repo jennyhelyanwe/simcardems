30994c112c253ba49f749f4967accb5d810e4b13
jennyhelyanwe <jenny.helyanwe@gmail.com>
2026-07-28 12:50:17 +0100

    New git submodule structure - fix starting docker

--- start_docker.sh
+++ start_docker.sh
@@ -1,5 +1,5 @@
 docker run --rm -it \
-  -v /Users/jenny/PycharmProjects/simcardems:/repo/simcardems-dev \
+  -v /Users/jenny/PycharmProjects/Cardiac-DT-Pipeline/simcardems:/repo/simcardems-dev \
   -w /repo/simcardems-dev/simulations \
   ghcr.io/computationalphysiology/simcardems:latest bash
  
